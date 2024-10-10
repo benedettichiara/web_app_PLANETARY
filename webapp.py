015a5dd8241c0952f204b1d398e024238bd18cf8
benedettichiara <127530314+benedettichiara@users.noreply.github.com>
2024-10-10 15:46:31 +0200

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import xarray as xr
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, BoundaryNorm
+import tempfile
+import plotly.express as px
+import plotly.graph_objects as go
+from datetime import datetime, timedelta
+import pystac_client
+import planetary_computer
+import geopandas as gpd
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import rioxarray
+
+# inizio scrittura codice
+
+def main():
+  # Streamlit interface for inputs
+  # TITOLO
+    st.title("ERA5  Planetary Data Download")
+  #date
+    start_date = st.date_input("Start date", value=datetime(1995, 1, 1)) # che nella choamata dell'utente potà essere cambiato
+    end_date = st.date_input("End date", value=datetime(1995, 12, 31))
+
+  # definizione del bounding box di visualizzazione iniziale
+  # tutte queste variabili potranno essere cambiate dall'utente
+    lat_min = st.number_input("Latitude Min:", value=-31.0)
+    lat_max = st.number_input("Latitude Max:", value=-29.0)
+    lon_min = st.number_input("Longitude Min:", value=26.0)
+    lon_max = st.number_input("Longitude Max:", value=29.0)
+    location = [lat_min, lat_max, lon_min, lon_max]
+    location1 = [lon_min, lat_min, lon_max, lat_max]
+    location_str = ', '.join(map(str, location1))
+    print(location_str)
+
+  # nomi delle variabili di ERA5 che vogliamo vengano tenute in considerazione
+   # var_ERA5 = st.selectbox( "ERA5variable", ('precipitation_amount_1hour_Accumulation', 'air_temperature_at_2_metres_1hour_Maximum', 'air_temperature_at_2_metres_1hour_Minimum','eastward_wind_at_10_metres','northward_wind_at_10_metres'),
+    #                         index=None,placeholder="Select Variable.",)
+
+    var_ERA5 = st.selectbox(
+    label="Select an ERA5 Variable",
+    options=[
+        'precipitation_amount_1hour_Accumulation', 
+        'air_temperature_at_2_metres_1hour_Maximum', 
+        'air_temperature_at_2_metres_1hour_Minimum',
+        'eastward_wind_at_10_metres', 
+        'northward_wind_at_10_metres'
+    ],
+    index=0,  # Default selection to the first variable
+    help="Choose the variable you wish to analyze from ERA5 data.")
+    st.write(f"Selected variable: {var_ERA5}")
+
+if __name__ == "__main__":
+    main()
